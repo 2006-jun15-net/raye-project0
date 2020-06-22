228ea6eef8b74b22d47a863cd1c3f734c0d33682
Ray Eichler <ray.eichler@yahoo.com>
2020-06-22 17:05:21 -0500

    Further work on project 0

--- Project0/Project0.library/Class1.cs
+++ Project0/Project0.library/Class1.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
 
 namespace Project0.library
 {
-    public class Class1
+    public class Store
     {
+        private int _storeNumber;
+        private string _storeStreetAddress;
+        private string _storeCity;
+        private string _storeState;
+        private int _storeZip;
+        private string _storePhone;
+        private string _storeWebAddress;
+        private string _storeEmail;
+        private string _storeManagerName;
     }
 }
